8ac96f1c4cb1b04723b5202c381f500df723baff
TimoSci <me@timosci.net>
2015-12-01 22:21:06 -0500

    made code more concise

--- deepop.rb
+++ deepop.rb
@@ -3,12 +3,11 @@ module Enumerable
 
   #Extentions for mixed data structures consisting of nested Arrays and Hashes
 
+
   # Like #each, but it loops through every Array and Hash in a nested Array/Hash data structure
   def each_deep
     deep =->(x){
-      if not Enumerable === x
-        yield x
-      else
+      if Enumerable === x
         x.each do |element|
           case x
           when Array
@@ -17,6 +16,8 @@ module Enumerable
             deep.call(element[1])
           end
         end
+      else
+        yield x
       end
     }
     deep.call(self)
@@ -27,23 +28,18 @@ module Enumerable
   # For an Array is maps the elements and for a Hash it maps the values
   def map_deep
     deep =->(x){
-      if not Enumerable === x
-        yield x
+      case x
+      when Array
+        x.map{|e| deep.call(e)}
+      when Hash
+        x.merge(x){|key,val| deep.call(val)}
       else
-          case x
-          when Array
-            x.map{|e| deep.call(e)}
-          when Hash
-            h={}
-            x.each do |key,value|
-              h[key] = deep.call(value)
-            end
-            h
-          end
+        yield x
       end
     }
     deep.call(self)
   end
+
 
   # Like #flatten, but it also flattens Hashes inside a nested Hash/Array data structure.
   # The values of Hashes are pushed into the output array and the keys are discarded
@@ -57,9 +53,9 @@ end
 
 
 
- ##DEMO
- #data_structure = [1,2,[31,32],4,[{a:51,b:52,c:[531,532,[5331,5332]]},{a:54,b:55}]]
+ # #DEMO
+ # data_structure = [1,2,[31,32],4,[{a:51,b:52,c:[531,532,[5331,5332]]},{a:54,b:55}]]
 
- #data_structure.each_deep{|x| puts x}
- #puts data_structure.map_deep{|x| x*10}.inspect
- #puts data_structure.trample.inspect
+ # data_structure.each_deep{|x| puts x}
+ # puts data_structure.map_deep{|x| x*10}.inspect
+ # puts data_structure.trample.inspect
