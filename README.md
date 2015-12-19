1d9c71e4ec0b259b861cb25019fc907b3546934d
TimoSci <me@timosci.net>
2015-12-19 13:34:36 -0500

    minor tweaks

--- README.md
+++ README.md
@@ -2,4 +2,16 @@
 
 My personal ruby swiss army knife
 
-This reposity contains methods, modules, and classes that I have created over the years
+This reposity contains methods, modules, and classes that I have created for my own usage.
+
+##How to use deepop.rb
+
+Save the file deepop.rb in the same folder as you application, then include it using `require_relative 'deepop'`` and your Enumerable class
+is monkey patched and ready to go
+
+Usage examples:
+
+```
+my_array = [1,[2,3],{a:1,b:2}]; puts array.trample`
+my_hash = {a:1,b:[2,3],c:[5,[6,7]]}; puts hash.trample`
+```
--- deepop.rb
+++ deepop.rb
@@ -1,45 +1,40 @@
-require 'pry'
 module Enumerable
 
   #Extentions for mixed data structures consisting of nested Arrays and Hashes
+  #---------------------------------------------------------------------------
 
-  # Generalized sender of iterator methods to Enumerable trees.
+  # Generalized recursive sender of iterator methods to Enumerable trees.
   def deep_send(iterator)
-    deep =->(x){
+    deep =->(x) do
       case x
       when Enumerable
         x.send(iterator){|e| deep.(e)}
       else
         yield x
       end
-    }
+    end
     deep.(self)
-
   end
 
   # Like #each, but it loops through every Array and Hash in a nested Array/Hash data structure.
   # For Hashes it iterates through the keys and for Arrays it iterates through the values.
-
   def each_deep
-    self.deep_send("each_values"){|x| yield x}
+    self.deep_send("each_values"){|e| yield e}
   end
 
   # Like #map, but it maps every element in a nested Hash/Array data structure.
   # For an Array is maps the elements and for a Hash it maps the values
-
   def map_deep
-    self.deep_send("map_values"){|x| yield x}
+    self.deep_send("map_values"){|e| yield e}
   end
 
   # A #map that works both for Arrays and Hashes. For Hashes it keeps the original keys.
   def map_values
     case self
     when Array
-      self.map{|x| yield x}
+      self.map{|e| yield e}
     when Hash
       self.map{|k,v| [k,(yield v)]}.to_h
-    else
-      nil
     end
   end
 
@@ -47,17 +42,29 @@ module Enumerable
   def each_values
     case self
     when Array
-      self.each{|x| yield x}
+      array = self
     when Hash
-      self.each{|_k,v| yield v}
-    else
-      nil
+      array = self.values
     end
+    array.each{|e| yield e}
+  end
+
+  # Like #flatten, but it also flattens Hashes inside a nested Hash/Array data structure.
+  # The values of Hashes are pushed into the output array and the keys are discarded
+  def trample
+    out = []
+    self.each_deep{|e| out << e}
+    out
+  end
+
+  # Like #include, but it searches through all Array elements and Hash values.
+  def include_deep?(input)
+    self.each_deep{|e| return true if e == input}
+    return false
   end
 
   # Selects a hash of all Hash sub-branches based on the key
   # Returns an array of the values of found branches
-  #
   def select_branches_by_key
     branches = []
     deep =->(x){
@@ -71,21 +78,7 @@ module Enumerable
       end
     }
     deep.(self)
-    branches 
-  end
-
-  # Like #flatten, but it also flattens Hashes inside a nested Hash/Array data structure.
-  # The values of Hashes are pushed into the output array and the keys are discarded
-  def trample
-    out = []
-    self.each_deep{|x| out << x}
-    out
-  end
-
-  # Like #include, but it searches through all Array elements and Hash values.
-  def include_deep?(input)
-    self.each_deep{|x| return true if x == input}
-    return false
+    branches
   end
 
 
@@ -93,8 +86,10 @@ end
 
 
 
- # #DEMO
-#  data_structure = [1,2,[31,32],4,[{a:51,b:52,c:[531,532,[5331,5332]]},{a:54,b:55}],6]
+ # # DEMO
+ # # ----
+ #
+ # data_structure = [1,2,[31,32],4,[{a:51,b:52,c:[531,532,[5331,5332]]},{a:54,b:55}],6]
  # puts "original data structure:"
  # puts data_structure.inspect
  # puts
@@ -112,3 +107,6 @@ end
  # puts
  # puts "find? 5332 somwhere in data structure"
  # puts data_structure.include_deep?(5332)
+ # puts
+ # puts "select all braches labeled ':a' in data structure"
+ # puts data_structure.select_branches_by_key{|k| k== :a}.inspect
